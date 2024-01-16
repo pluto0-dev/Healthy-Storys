e2c2becb78e067dfc107efc84d1b04290830e818
pluto0-dev <nick.poommipat@gmail.com>
2024-01-17 04:38:10 +0700

    no message

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,9 @@
-'use client'
-import React, { useState, useEffect } from "react";
+
 import User from "@/components/user/User";
-import { Directus } from "@directus/sdk";
+
 
 const Home = () => {
-  const directus = new Directus("http://localhost:8055");
-  const [user, setUser] = useState();
-  const [searchValue, setSearchValue] = useState("");
-
-  const fetchUser = async () => {
-    try {
-      const userResponse = await directus.items("content").readByQuery({
-        filter: { username: searchValue },
-      });
-      setUser(userResponse.data[0]);
-      console.log(userResponse.data);
-    } catch (error) {
-      console.error("Error fetching content:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUser();
-  }, [searchValue]);
-
+  
   return (
     <>
       <div className="flex flex-col items-center justify-start h-screen mt-28">
